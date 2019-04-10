e392295e8f59e26fd2cd6809c0fd212f67dbe575
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-10 19:34:04 +0200

    Draw snake on canvas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const canvasBorderColor = '#F22613';
 const canvasBackgroundColor = '#264348';
+const snakeColor = '#006442';
+const snakeBorderColor = '#000';
 
 const canvas = document.getElementById('canvas');
 const drawingContext = canvas.getContext('2d');
@@ -8,3 +10,24 @@ drawingContext.fillStyle = canvasBackgroundColor;
 drawingContext.strokestyle = canvasBorderColor;
 drawingContext.fillRect(0, 0, canvas.width, canvas.height);
 drawingContext.strokeRect(0, 0, canvas.width, canvas.height);
+
+let snake = [
+  { x: 400, y: 400 },
+  { x: 380, y: 400 },
+  { x: 360, y: 400 },
+]
+
+const drawSnakePart = snakePart => {
+  drawingContext.fillStyle = snakeColor;
+  drawingContext.strokestyle = snakeBorderColor;
+
+  drawingContext.fillRect(snakePart.x, snakePart.y, 20, 20);
+  drawingContext.strokeRect(snakePart.x, snakePart.y, 20, 20);
+}
+
+const drawSnake = (() => {
+  snake.forEach(snakePart => {
+    console.log(snakePart);
+    drawSnakePart(snakePart);
+  });
+})();
