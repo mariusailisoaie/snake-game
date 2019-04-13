133a256f2325e86c541f56c4cee7c28c68107697
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-13 10:56:25 +0200

    Fix snake init position, pos update, walking through walls

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const canvasBorderColor = '#F22613';
 const canvasBackgroundColor = '#95A5A6';
 const snakeColor = '#006442';
 const snakeBorderColor = '#000';
-const snakeHeadColor = '#CA6924';
-
-let dx = 20;
-let dy = 0;
 
 const canvas = document.getElementById('canvas');
 const drawingContext = canvas.getContext('2d');
+
+let dx = 20, dy = 0;
+
+let snake = [
+  { x: 200, y: 0 },
+  { x: 180, y: 0 },
+  { x: 160, y: 0 },
+  { x: 140, y: 0 },
+]
 
 const paintCanvas = () => {
   drawingContext.fillStyle = canvasBackgroundColor;
@@ -17,25 +22,12 @@ const paintCanvas = () => {
   drawingContext.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
-let snake = [
-  { x: 180, y: 200 },
-  { x: 160, y: 200 },
-  { x: 140, y: 200 },
-  { x: 120, y: 200 },
-]
-
 const drawSnakePart = snakePart => {
   drawingContext.fillStyle = snakeColor;
   drawingContext.strokestyle = snakeBorderColor;
   
-  drawingContext.fillRect(snakePart.x, snakePart.y, 20, 20);
-  drawingContext.strokeRect(snakePart.x, snakePart.y, 20, 20);
-  
-  drawingContext.fillStyle = snakeHeadColor;
-  drawingContext.strokestyle = snakeBorderColor;
-  
-  drawingContext.fillRect(snake[0].x, snake[0].y, 20, 20);
-  drawingContext.strokeRect(snake[0].x, snake[0].y, 20, 20);
+  drawingContext.fillRect(snakePart.x - 20, snakePart.y, 20, 20);
+  drawingContext.strokeRect(snakePart.x - 20, snakePart.y, 20, 20);
 }
 
 const drawSnake = () => {
@@ -44,16 +36,15 @@ const drawSnake = () => {
   });
 };
 
-paintCanvas();
-drawSnake();
-
 const snakeMove = () => {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy }
+	// console.log('log: snakeMove -> head', head);
+	console.log('log: snakeMove -> snake[0].x', { x: snake[0].x, y: snake[0].y });
 
-  if (head.x === 400) {
-    head.x = 0;
-  } else if(head.x === -20) {
-    head.x = 380;
+  if (head.x === 420) {
+    head.x = 20;
+  } else if(head.x === 0) {
+    head.x = 400;
   } else if(head.y === 400) {
     head.y = 0;
   } else if(head.y === -20) {
@@ -63,6 +54,10 @@ const snakeMove = () => {
   snake.unshift(head);
   snake.pop();
 }
+
+paintCanvas();
+drawSnake();
+snakeMove();
 
 const arrowKeyPressed = key => {
   if (key === 'ArrowUp' && dy !== 20) {
@@ -85,12 +80,11 @@ const arrowKeyPressed = key => {
 }
 
 document.addEventListener('keydown', e => {
-  console.log(e.key);
   arrowKeyPressed(e.key)
 });
 
-setInterval(() => {
-  paintCanvas();
-  drawSnake();
-  snakeMove();
-}, 1000);
+// setInterval(() => {
+//   paintCanvas();
+//   drawSnake();
+//   snakeMove();
+// }, 100);
