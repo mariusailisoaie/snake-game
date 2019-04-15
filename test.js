1699c938c496d0487f571418f63d8afe69f4fc63
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-15 21:43:50 +0200

    Test array splice

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
-const randomNumbersArr = [];
+const numbers = [];
+
+for(let i = 0; i < 10; i++) {
+  numbers.push(i + 1);
+}
+
+const spliced = numbers.splice(0, 3);
+
+console.log(numbers);
+console.log(spliced);
+
+/* const randomNumbersArr = [];
 
 for(let i = 0; i < 100; i++) {
   let randomNumber = Math.floor(Math.random() * 19 + 1);
-  // console.log(randomNumber);
   randomNumbersArr.push(randomNumber * 20);
 }
 
 console.log(randomNumbersArr.sort((a, b) => a - b));
+*/
